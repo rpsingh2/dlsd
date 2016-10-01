0d0a8f385edc8b6e4fd177f83326d4b468af608e
Zach Thomas <zacherythomas12@gmail.com>
2016-10-01 02:16:18 +0000

    Update MWS and add Screenshot

--- code/5/MaxWalkSat.py
+++ code/5/MaxWalkSat.py
@@ -1,6 +1,19 @@
 from Osyczka2 import Osyczka2
 from random import *
 import copy
+import sys
+
+"""
+
+How to read output:
+. = iteration complete
+! = new best entropy found
+r = rolling (probably not the technical term (local search?))
+j = jumping to random location
+
+float at end of the line is the current best entropy
+
+"""
 
 model = Osyczka2()
 
@@ -9,21 +22,25 @@ p = 0.5
 
 best_model = copy.deepcopy(model)
 
-for x in range(trials):
-    #model = Osyczka2()
+def say(x):
+    sys.stdout.write(str(x)); sys.stdout.flush()
 
-    #print str(model.energy()) + " " + str(best_model.energy())
+for x in range(trials):
     if model.energy() < best_model.energy():
         best_model = copy.deepcopy(model)
-        print "be: " + str(best_model.energy())
+        say("!")
         if best_model.energy() == 0.0: break
     if p < random():
         model.mutate_rand_x()
-        print "jump " + str(model.energy())
+        say("j")
     else:
         model = model.roll()
-        print "roll " + str(model.energy())
-
-print "trials " + str(x)
-print best_model.energy()
-print best_model.to_str()
+        say("r")
+    say(".")
+    if x % 50 == 0: say("|" + str(best_model.energy()) + "\n")
+    #print str(model.f1()) + "," + str(model.f2())
+print
+print
+print "number of trials:" + str(x)
+print "best entropy: " + str(best_model.energy())
+print "best x vals: " + str(best_model.to_str())
