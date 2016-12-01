cb467e7d1ea8c95ce93e70c6c9528ec5862fa22f
ZacheryThomas <zacherythomas12@gmail.com>
2016-11-30 22:06:28 -0500

    Delete Individual.py

--- code/8/Individual.py
+++
@@ -1,7 +0,0 @@
-class Individual(object):
-    def __init__(self):
-        self.rank = None
-        self.dominated_solutions = set()
-        self.features = None
-        self.decisions = None
-        self.dominates = None
